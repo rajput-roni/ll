547b55966715313b3c49d436dc9a23c26eddb162
rajput-roni <rajnath37857@gmail.com>
2025-03-31 08:09:27 +0530

    Update and rename offline_sms_gsm to offline_sms_sender.py

---
+++ offline_sms_sender.py
@@ -0,0 +1,8 @@
+mkdir offline-sms-sender
+cd offline-sms-sender
+git init
+cp /path/to/offline_sms_sender.py .
+git add offline_sms_sender.py
+git commit -m "Initial commit: Added complete offline SMS sender script"
+git remote add origin https://github.com/username/offline-sms-sender.git
+git push -u origin master
